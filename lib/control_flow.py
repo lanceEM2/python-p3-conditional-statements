6fa3c094549f57393aadd35634b65eadabfb78e6
Dylan <dylanlancekemboi@gmail.com>
2023-12-07 20:56:11 +0300

    Completed Lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,60 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
+admin_login("admin", "1235")
+print(admin_login)
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
+# Example usage:
+print(hows_the_weather(90))      
+
 
 def fizzbuzz(num):
     # your code here
     pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
+
+fizzbuzz(9)
+print(fizzbuzz)
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    match operation:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print("Cannot divide by zero!")
+                return None
+        case _:
+            print("Invalid operation!")
+            return None
+
+calculator("-", 4, 2)
